86d90c93fec1cb89c58bc30a3e50dee4f61843e7
Toby Drane <tobydrane@gmail.com>
2019-01-14 12:31:55 +0000

    foursquare

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app.socials.flikr import getFlickr
 from app.socials.twitter import getTweets
 from app.socials.instagram import getInstagram
 from app.news.news import getNews
+import app.socials.foursquare as foursquare
 
 @app.route('/')
 @app.route('/index')
@@ -33,6 +34,7 @@ def news(search_param):
 #   /twitter/SOME_SEARCH_PARAM
 #   /flikr/SOME_SEARCH_PARAM
 #   /instagram/SOME_SEACH_PARAM
+#   /foursquare/searchvenue/SOME_SEARCH_PARAM
 @app.route('/social/twitter/<string:search_param>')
 def twitter(search_param):
   tweets = getTweets(search_param)
@@ -47,3 +49,13 @@ def flikr(search_param):
 def instagram(search_param):
   instagram = getInstagram(search_param)
   return jsonify(instagram)
+
+@app.route('/social/foursquare/searchvenue/<string:search_param>')
+def foursquareSearchVenue(search_param):
+  venues = foursquare.venuSearch(search_param)
+  return jsonify(venues)
+
+@app.route('/social/foursquare/venuPhotos/<string:venu_id>')
+def foursquareVenuPhotos(venu_id):
+  venues = foursquare.venuPhotos(venu_id)
+  return jsonify(venues)
